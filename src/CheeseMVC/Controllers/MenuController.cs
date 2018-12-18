4a4c0fcd3580e6c5aea8e25344c79b2519f34e5b
Joshua Ortmann <joshuaortmann@gmail.com>
2018-12-17 19:41:25 -0500

    Changes

---
+++ src/CheeseMVC/Controllers/MenuController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CheeseMVC.Data;
+using CheeseMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CheeseMVC.Controllers
+{
+    public class MenuController : Controller
+    {
+        public IActionResult Index()
+        {
+            IList<Menu> menus = context.Menus.Include(c => c.Name).ToList();
+            return View();
+        }
+
+        private readonly CheeseDbContext context;
+
+        public MenuController(CheeseDbContext dbContext)
+        {
+            context = dbContext;
+        }
+    }
+}
