285843660e4fe1335f7ecbe17d04259133c0c6e0
Joshua Ortmann <joshuaortmann@gmail.com>
2018-12-17 20:14:31 -0500

    Created ViewMenuModel.cs

--- src/CheeseMVC/Controllers/MenuController.cs
+++ src/CheeseMVC/Controllers/MenuController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using CheeseMVC.Data;
 using CheeseMVC.Models;
+using CheeseMVC.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -13,8 +14,32 @@ namespace CheeseMVC.Controllers
     {
         public IActionResult Index()
         {
+            ViewBag.Title = "Menus";
             IList<Menu> menus = context.Menus.Include(c => c.Name).ToList();
-            return View();
+            return View(menus);
+        }
+
+        public IActionResult Add()
+        {
+            return View("Add");
+        }
+
+        [HttpPost]
+        public IActionResult Add(AddMenuViewModel addMenuViewModel)
+        {
+            if (addMenuViewModel.Name != null)
+            {
+                Menu newMenu = new Menu
+                {
+                    Name = addMenuViewModel.Name
+                };
+                context.Menus.Add(newMenu);
+                context.SaveChanges();
+                return Redirect("/Menu/ViewMenu/" + newMenu.ID);
+            } else
+            {
+                return View("Add");
+            }
         }
 
         private readonly CheeseDbContext context;
