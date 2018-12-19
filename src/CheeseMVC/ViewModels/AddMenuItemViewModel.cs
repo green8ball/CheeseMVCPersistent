70e37d720048de9e6d798859445d331bc7b704c5
Joshua Ortmann <joshuaortmann@gmail.com>
2018-12-19 18:33:20 -0500

    Added AddMenuItemViewModel.cs

---
+++ src/CheeseMVC/ViewModels/AddMenuItemViewModel.cs
@@ -0,0 +1,17 @@
+﻿using CheeseMVC.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CheeseMVC.ViewModels
+{
+    public class AddMenuItemViewModel
+    {
+        public int cheeseID { get; set; }
+        public int menuID { get; set; }
+        public Menu Menu { get; set; }
+        public List<SelectListItem> Cheeses { get; set; }
+    }
+}
